6181b6261b5c8ec05529d5d97bb495229eb68134
Saki-htr <saki.hattori0116@gmail.com>
2021-04-10 13:22:47 +0900

    更新後のリダイレクト先をユーザー詳細画面に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  #ログインしていない時、ログイン画面にリダイレクト
-  before_action :authenticate_user!, only: :index
-
   def index
     @users = User.order(:id).page(params[:page])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  devise_for :users
+  # registrations_controller.rbで記述する内容を有効にする
+  devise_for :users,controllers: {
+    registrations: 'users/registrations'
+  }
+  # get 'users/:id', to: 'users#show', as: 'user_page'
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+
+
   #users=index,users#showのルーティング追加
   resources :users, :only => [:index, :show]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # root to: 'users#index'
 end
