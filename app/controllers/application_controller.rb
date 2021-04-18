9c7b122d8d9a104db6d33f7fb7483c05cd6db123
Saki-htr <saki.hattori0116@gmail.com>
2021-04-18 13:44:59 +0900

    パスをURLヘルパーに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   def after_sign_up_path_for(_resource)
-    '/users'
+    users_path
   end
 
   def after_sign_in_path_for(_resource)
-    '/users'
+    users_path
   end
 
   protected
