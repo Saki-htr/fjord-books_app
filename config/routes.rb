f99d51af30562e1986216f77aea197d888f637d9
Saki-htr <saki.hattori0116@gmail.com>
2021-04-18 20:54:45 +0900

    ハッシュロケットをシンボルに書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
 
   #users=index,users#showのルーティング追加
-  resources :users, :only => [:index, :show]
+  resources :users, only: [:index, :show]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
