3d53b9cd07c318fd80c22fa41aad837ec3edff9d
Saki-htr <saki.hattori0116@gmail.com>
2021-04-18 19:53:12 +0900

    追加したカラムにNotNull制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_040005) do
+ActiveRecord::Schema.define(version: 2021_04_18_103740) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -29,10 +29,10 @@ ActiveRecord::Schema.define(version: 2021_03_27_040005) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "username"
-    t.integer "postal_code"
-    t.text "address"
-    t.text "description"
+    t.string "username", null: false
+    t.integer "postal_code", null: false
+    t.text "address", null: false
+    t.text "description", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
