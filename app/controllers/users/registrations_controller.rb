769358fc2d8d2ecf4b08310ebf57d00de0720b69
Saki-htr <saki.hattori0116@gmail.com>
2021-04-19 18:41:16 +0900

    誤ったコードを削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,8 +6,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_update_path_for(resource)
     user_path(resource)
   end
-
-  def after_sign_out_path_for(_resource)
-    new_user_session_path
-  end
 end
