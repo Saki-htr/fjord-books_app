14db253c2c026e16a498ccaec524766d0d02b744
Saki-htr <saki.hattori0116@gmail.com>
2021-06-17 17:59:09 +0900

    commentモデルにbook_idを外部キーとして追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_082924) do
+ActiveRecord::Schema.define(version: 2021_06_17_085300) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2021_06_17_082924) do
     t.integer "report_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "book_id"
+    t.index ["book_id"], name: "index_comments_on_book_id"
     t.index ["report_id"], name: "index_comments_on_report_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -100,7 +102,8 @@ ActiveRecord::Schema.define(version: 2021_06_17_082924) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "books", "users"git
+  add_foreign_key "books", "users"
+  add_foreign_key "comments", "books"
   add_foreign_key "comments", "reports"
   add_foreign_key "comments", "users"
   add_foreign_key "reports", "users"
