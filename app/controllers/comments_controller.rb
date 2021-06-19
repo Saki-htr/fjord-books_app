3555ab0711f779e5218000d47dc60ee2e59ef86e
Saki-htr <saki.hattori0116@gmail.com>
2021-06-19 12:26:09 +0900

    mergeメソッドをcomment_paramsに移動

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,16 @@ class CommentsController < ApplicationController
   def create
     resource, id = request.path.split('/')[1,2]
     @commentable = resource.singularize.classify.constantize.find(id)
-    @comment = @commentable.comments.new(comment_params.merge(user: current_user))
+    @comment = @commentable.comments.new(comment_params)
     if @comment.save
       redirect_to [@commentable, @comments]
     else
-      render :new
+      render :create
     end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content).merge(user: current_user)
   end
 end
