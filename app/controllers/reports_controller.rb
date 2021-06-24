e93f07486502902f20679e8b90d39f5993f6f505
Saki-htr <saki.hattori0116@gmail.com>
2021-06-24 18:41:08 +0900

    user_idをpermitすべきでないため削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -60,6 +60,6 @@ class ReportsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def report_params
-    params.require(:report).permit(:title, :content, :user_id)
+    params.require(:report).permit(:title, :content)
   end
 end
