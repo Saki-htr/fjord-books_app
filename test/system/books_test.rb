07aa6ea9bcd9003c562c17372a4304b49c4ceb97
Saki-htr <saki.hattori0116@gmail.com>
2021-07-05 19:20:07 +0900

    ログイン処理のシステムテスト

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -5,6 +5,11 @@ require 'application_system_test_case'
 class BooksTest < ApplicationSystemTestCase
   setup do
     @book = books(:one)
+
+    visit root_path
+    fill_in "Eメール", with: "alice@example.com"
+    fill_in "パスワード", with: "password"
+    click_button "ログイン"
   end
 
   test 'visiting the index' do
@@ -21,7 +26,7 @@ class BooksTest < ApplicationSystemTestCase
     click_on 'Create Book'
 
     assert_text 'Book was successfully created'
-    click_on 'Back'
+    # click_on 'Back'
   end
 
   test 'updating a Book' do
@@ -33,15 +38,15 @@ class BooksTest < ApplicationSystemTestCase
     click_on 'Update Book'
 
     assert_text 'Book was successfully updated'
-    click_on 'Back'
+    # click_on 'Back'
   end
 
   test 'destroying a Book' do
     visit books_url
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on '削除', match: :first
     end
 
-    assert_text 'Book was successfully destroyed'
+    assert_text '本が削除されました'
   end
 end
