94db7152422f6bc79d524e4a371a35bc193a1cfa
Saki-htr <saki.hattori0116@gmail.com>
2021-07-08 15:29:55 +0900

    booksのシステムテストのエラーを解消

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'application_system_test_case'
+require "application_system_test_case"
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
+    @book = books(:onepiece)
 
     visit root_path
     fill_in "Eメール", with: "alice@example.com"
@@ -12,41 +12,44 @@ class BooksTest < ApplicationSystemTestCase
     click_button "ログイン"
   end
 
-  test 'visiting the index' do
-    visit books_url
-    assert_selector 'h1', text: 'Books'
+  test "visiting the index" do
+    visit books_path
+    assert_selector "h1", text: "本"
   end
 
-  test 'creating a Book' do
-    visit books_url
-    click_on 'New Book'
+  test "creating a Book" do
+    visit books_path
+    click_on "新規作成"
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in "タイトル", with: "Ruby超入門"
+    fill_in "メモ", with: "分かりやすいです！"
+    fill_in "著者", with: "五十嵐邦明"
+    click_button "登録する"
 
-    assert_text 'Book was successfully created'
-    # click_on 'Back'
+    assert_text "本が作成されました。"
+    assert_text "Ruby超入門"
+    assert_text "分かりやすいです！"
+    assert_text "五十嵐邦明"
   end
 
   test 'updating a Book' do
-    visit books_url
-    click_on 'Edit', match: :first
+    visit books_path
+    click_on "編集"
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+    fill_in "タイトル", with: @book.memo
+    fill_in "メモ", with: @book.title
+    fill_in "著者", with: @book.author
+    click_on "更新する"
 
-    assert_text 'Book was successfully updated'
-    # click_on 'Back'
+    assert_text "本が更新されました。"
   end
 
   test 'destroying a Book' do
-    visit books_url
+    visit books_path
     page.accept_confirm do
-      click_on '削除', match: :first
+      click_on "削除", match: :first
     end
 
-    assert_text '本が削除されました'
+    assert_text "本が削除されました。"
   end
 end
